0d68a36e3d5584c5ad2a7aaa531e7c0d480055be
monki1 <114178697+monki1@users.noreply.github.com>
2023-09-06 18:54:22 -0400

    √ registerCommands.js

---
+++ registerCommands.js
@@ -0,0 +1,25 @@
+import { REST, Routes } from 'discord.js';
+import {config} from "dotenv";
+const { DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID } = config().parsed;
+
+
+
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
+
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
