fe92729d058e7de46c189536107e4d849420818b
monki1 <114178697+monki1@users.noreply.github.com>
2023-09-08 05:09:36 -0400

    √ update command

--- message/heyRudolf/index.js
+++ message/heyRudolf/index.js
@@ -1,9 +1,10 @@
 import hf from "../../gpt/hf.js";
 import novacord from "../../gpt/novacord.js";
+const command = "rudolf";
 
 export default async function (message) {
-    if (message.content.includes("hey rudolf")) {
-        const input = message.content.replace("hey rudolf", "");
+    if (message.content.includes(command)) {
+        const input = message.content.replace(command, "");
         // message.reply(await hf(input));
         message.reply(await novacord(input));
     }
